0e0b72e31de473c9f99fbb4d5546a2beffe0a950
Thillaishanmugam1998 <shansenthil72@gmail.com>
2024-08-01 18:24:09 -0700

    Added Arrays Tutorials

---
+++ 02_C-Sharp CodeBase/11_ARRAYS/1_ARRAYS_INTRO/1_Array.cs
@@ -0,0 +1,109 @@
+﻿/*  C# Arrays with Examples:
+ *  ------------------------
+ *  In c#, Arrays are useful for storing multiple elements of the same data type at contiguous memory locations. 
+    It will store a fixed number of elements sequentially based on the predefined number of items.
+ * 
+ * An array can start storing the values from index 0. 
+   If we have an array with n elements, it will start storing the elements from index 0 to n-1.
+ */
+using System;
+
+namespace _1_ARRAYS_INTRO
+{
+    class _1_Array
+    {
+        //1. Array Declaration
+        public void ArrayDeclaration()
+        {
+            // Syntax: type_data[] arr_name;
+            #region Array Declaration:
+            /*
+              In c#, the array elements can be of any type, and by default,
+              the values of numeric array elements are set to zero, and the reference elements are set to null.
+             */
+            #endregion
+
+            //Store Int values only
+            int[] arr_int;
+            //Store String values only
+            string[] arr_string;
+            //Store Float values only
+            float[] arr_float;
+            //Store Double values only
+            double[] arr_double;
+
+        }
+        
+        //2.Array Initialization
+        public void ArrayInitialization()
+        {
+            #region Array Initialization:
+            /*
+             In c#, Arrays can be initialized by creating an instance of the array with a new keyword. Using a new keyword, 
+             we can declare and initialize an array simultaneously based on our requirements.
+            */
+            #endregion
+
+            //  1.  Declaring and Initialization Array with size of 5
+            int[] arr_int = new int[5];
+            //  2.  Declaring and Initialization Array and Assign values at same time
+            int[] arr_int_2 = new int[5] { 1, 2, 3, 4, 5 };
+            //  3.  Initialize with 5 elements will indicate the size of an array
+            int[] arr_int_3 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //  4.  Another way to initialize an array without the size
+            int[] arr_int_4 = { 1, 2, 3, 4, 5 };
+            //  5.  Declare an array without initialization
+            int[] arr_int_5;
+            arr_int_5 = new int[] { 1, 2, 3, 4, 5 };
+            //  6.  Initialization array element using index
+            int[] arr_int_6 = new int[2];
+            arr_int_6[0] = 1;
+            arr_int_6[1] = 2;
+
+            #region Array Without Size and New Keyword throw compile-time Error:
+            /*
+            // Error. Initialize an array without size
+            int[] array = new int[];
+            // Error. Initialize an array without a new keyword
+            int[] array1;
+            array1 = { 1, 2, 3, 4, 5 };
+            */
+            #endregion
+        }
+
+        //3.Array Element Accessing
+        //In c#, we can access array elements using for loop or foreach loop or with particular index numbers.
+        public void ArrayElementAccessByIndex()
+        {
+            int[] arr1 = new int[5] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Index [0]: {0}", arr1[0]);
+            Console.WriteLine("Index [1]: {0}", arr1[1]);
+            Console.WriteLine("Index [2]: {0}", arr1[2]);
+            Console.WriteLine("Index [3]: {0}", arr1[3]);
+            Console.WriteLine("Index [4]: {0}", arr1[4]);
+            Console.ReadLine();
+        }
+
+        //3.1.Array Element Accessing By For Loop:
+        public void ArrayElementAccessByForLoop()
+        {
+            int[] arr1 = new int[5] { 1, 2, 3, 4, 5 };
+            for(int i=0;i<arr1.Length;i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+            Console.ReadLine();
+        }
+
+        //3.2.Array Element Accessing By For Each Loop:
+        public void ArrayElementAccessByForEachLoop()
+        {
+            int[] arr1 = new int[5] { 1, 2, 3, 4, 5 };
+            foreach(int number in arr1)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02_C-Sharp CodeBase/11_ARRAYS/1_ARRAYS_INTRO/2_Array_Class.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_ARRAYS_INTRO
+{
+    class _2_Array_Class
+    {
+        #region Array Class:
+        /*
+           * In c#, we have a class called Array, and it will act as a base class for all the arrays in the common language runtime (CLR). 
+             The Array class provides methods for creating, manipulating, searching, and sorting arrays.
+            
+           * For example, by using the Sort or Copy methods of the Array class, 
+            we can sort the elements of an array and copy the elements of one array to another based on our requirements. 
+         */
+        #endregion
+
+        //1.Array.Copy:
+        public void ArrayCopy()
+        {
+            int[] source_arr      = new int[5] { 5, 2, 4, 1, 3 };
+            int[] destination_arr = new int[5];
+            Array.Copy(source_arr, destination_arr, source_arr.Length);
+            foreach (int i in destination_arr)
+            {
+                Console.WriteLine(i);    //5,2,4,1,3
+            }
+        }
+
+        //2.Array.Reverse:
+        public void ArrayReverse()
+        {
+            int[] source_arr = new int[5] { 5, 2, 4, 1, 3 };
+            Array.Reverse(source_arr);
+            foreach (int i in source_arr)
+            {
+                Console.WriteLine(i);    //3,1,4,2,5
+            }
+        }
+
+        //3. Array.Sort
+        public void ArraySort()
+        {
+            int[] numbers = { 5, 3, 8, 1, 2 };
+            Array.Sort(numbers);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number); // Output: 1 2 3 5 8
+            }
+        }
+
+        //4. Array.IndexOf
+        public void ArrayIndexOf()
+        {
+            string[] fruits = { "Apple", "Banana", "Cherry", "Date" };
+            int index = Array.IndexOf(fruits, "Cherry");
+            Console.WriteLine(index); // Output: 2
+        }
+
+        //5. Array.Clear
+        public void ArrayClear()
+        {
+            // Sets a range of elements in the array to the default value of each element type.
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Array.Clear(numbers, 1, 2);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number); // Output: 1 0 0 4 5
+            }
+
+        }
+
+        //6. Array.Resize
+        public void ArrayResize()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Array.Resize(ref numbers, 7);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number); // Output: 1 2 3 4 5 0 0
+            }
+        }
+
+        //7. Array.Find
+        public void ArrayFind()
+        {
+            // Searches for an element that matches the conditions defined by a specified predicate
+            // and returns the first occurrence.
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int result = Array.Find(numbers, n => n > 3);
+            Console.WriteLine(result); // Output: 4
+        }
+
+        //8. Array.Exists
+        public void ArrayExists()
+        {
+            // Determines whether the array contains elements that match the conditions
+            // defined by a specified predicate.
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            bool exists = Array.Exists(numbers, n => n > 3);
+            Console.WriteLine(exists); // Output: True
+        }
+    }
+}
