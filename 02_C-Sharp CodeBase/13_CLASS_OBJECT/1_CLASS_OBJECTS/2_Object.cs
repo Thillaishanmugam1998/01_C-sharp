5665c881288b099e00ca057769a0f9a816860074
Thillaishanmugam1998 <shansenthil72@gmail.com>
2024-08-22 18:12:09 -0700

    NEW COMMIT

---
+++ 02_C-Sharp CodeBase/13_CLASS_OBJECT/1_CLASS_OBJECTS/2_Object.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_CLASS_OBJECTS
+{
+    class _2_Object
+    {
+        #region OBJECT:
+        /*
+         * In c#, Object is an instance of a class that can be used to access the data members and member functions of a class.
+         */
+        #endregion
+
+        //Creating Objects in C#
+        /*
+         * Users user = new Users();
+         */
+
+        #region Example
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Users user = new Users("Suresh Dasari", 30);
+                user.GetUserDetails();
+                Console.WriteLine("Press Enter Key to Exit..");
+                Console.ReadLine();
+            }
+        }
+        public class Users
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Users(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            public void GetUserDetails()
+            {
+                Console.WriteLine("Name: {0}, Age: {1}", Name, Age);
+            }
+        }
+        #endregion
+    }
+}
---
+++ 02_C-Sharp CodeBase/14_CLASS OBJECTS/Solution1/1_Structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
